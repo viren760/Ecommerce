35976da07f7a801b23b8cd9636619108102536fd
Virender negi <58510282+viren760@users.noreply.github.com>
2021-04-05 18:22:20 +0530

    Update Cart.js

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,13 +1,19 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Cart = () => {
     return (
         <div className="container2">
             <div className="cart-container">
                <h2 style={{fontWeight:'bold', fontFamily:'sans-serif',  marginTop:'100px'}}> Shopping Cart </h2> 
+                   
+            </div>
+
+            <div style={{textAlign:'center'}}>
+              <Link to="/checkout"> <button className="btn btn-primary"> Checkout </button> </Link>
             </div>
         </div>
     )
 }
 
-export default Cart
+export default Cart;
